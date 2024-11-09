c88bc99bc115a133b9282ae002d0312b56a00be6
Andrey-444 <jermandrey@gmail.com>
2024-11-09 18:50:48 -0500

    javascript

--- js/slider.js
+++ js/slider.js
@@ -1,3 +1,17 @@
-var myCarousel = document.querySelector('#myCarousel')
-var carousel = new bootstrap.Carousel(myCarousel)
+const tarjetas = document.querySelectorAll('.tarjeta');
+
+tarjetas[0].style.backgroundImage = "url(assets/tupacgra.jpg)"; // Cambiar el color de la primera tarjeta
+tarjetas[1].style.backgroundImage = "url(assets/cartigra.webp)"; // Cambiar el color de la segunda tarjeta
+tarjetas[2].style.backgroundImage = "url(assets/tylergra.jpg)";
+
+// Opcional: añadir propiedades de estilo adicionales
+tarjetas[0].style.backgroundSize = "cover"; // Ajustar la imagen al tamaño de la tarjeta
+tarjetas[0].style.backgroundPosition = "center"; // Centrar la imagen
+
+
+tarjetas[1].style.backgroundSize = "cover"; // Ajustar la imagen al tamaño de la tarjeta
+tarjetas[1].style.backgroundPosition = "center"; // Centrar la imagen
+
+tarjetas[2].style.backgroundSize = "cover"; // Ajustar la imagen al tamaño de la tarjeta
+tarjetas[2].style.backgroundPosition = "center"; // Centrar la imagen
 
